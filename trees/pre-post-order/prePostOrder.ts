d4f8c122d8a029c0c9f10fd5499558055c03ff90
Nathan Rydel <nathanrydel@gmail.com>
2024-02-28 16:56:24 -0700

    Implemented preOrder
    - tests passing

--- trees/pre-post-order/prePostOrder.ts
+++ trees/pre-post-order/prePostOrder.ts
@@ -3,8 +3,16 @@ import { BNodeNum } from "../common/binary-search-tree";
 /** dfsPreOrder(): Traverse from the invoking node using pre-order DFS.
  * Returns an array of visited nodes. */
 
-function preOrder(node: BNodeNum | null): number[] {
-  return [42];
+function preOrder(node: TreeNodeNum | null): number[] {
+  if (node === null) return [];
+
+  const order = [node.val];
+
+  for (const child of node.children) {
+    order.push(...preOrder(child));
+  }
+
+  return order;
 }
 
 
