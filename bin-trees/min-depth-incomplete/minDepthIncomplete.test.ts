874f5bd62fe0e9af637512bfb194842f991bd496
Eric Huie <erichuie97@gmail.com>
2024-02-28 16:42:50 -0800

    working on binary min depth incomplete trees

--- bin-trees/min-depth-incomplete/minDepthIncomplete.test.ts
+++ bin-trees/min-depth-incomplete/minDepthIncomplete.test.ts
@@ -1,15 +1,16 @@
 import { expect, it } from "vitest";
 import { largeTree, longTree, smallTree } from "../common/bintree.test";
-import { minDepthToIncomplete } from "./minDepthIncomplete";
+import { minDepthToIncompleteNode } from "./minDepthIncomplete";
 
 it("handles simple trees", function () {
-  expect(minDepthToIncomplete(smallTree.root!)).toBe(2);
+  debugger;
+  expect(minDepthToIncompleteNode(smallTree.root!)).toBe(2);
 });
 
 it("handles more complex trees", function () {
-  expect(minDepthToIncomplete(largeTree.root!)).toBe(2);
+  expect(minDepthToIncompleteNode(largeTree.root!)).toBe(2);
 });
 
 it("handles long tree", function () {
-  expect(minDepthToIncomplete(longTree.root!)).toBe(2);
+  expect(minDepthToIncompleteNode(longTree.root!)).toBe(2);
 });
--- bin-trees/min-depth-incomplete/minDepthIncomplete.ts
+++ bin-trees/min-depth-incomplete/minDepthIncomplete.ts
@@ -6,7 +6,22 @@ import { BNodeNum } from "../common/bintree";
  */
 
 function minDepthToIncompleteNode(node: BNodeNum): number {
-  return 42;
+  if(node.lnode === null) return 0;
+  if(node.rnode === null) return 0;
+
+  let minLeftNodeDepth = 0;
+  let minRightNodeDepth = 0;
+
+
+  minLeftNodeDepth =
+    (Math.max(minDepthToIncompleteNode(node.lnode), minLeftNodeDepth));
+    debugger;
+  minRightNodeDepth =
+    (Math.max(minDepthToIncompleteNode(node.rnode), minRightNodeDepth));
+    debugger;
+
+  return Math.min(minLeftNodeDepth, minRightNodeDepth) + 1;
+
 }
 
 export { minDepthToIncompleteNode };
