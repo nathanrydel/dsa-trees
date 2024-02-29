a16439d651d381bba45957a30466803658e2b3c9
Nathan Rydel <nathanrydel@gmail.com>
2024-02-28 17:03:28 -0700

    updated implementation
    - added childDepth var for easier tracking
    - updated base case to be at top of fn

--- trees/max-depth/maxDepth.ts
+++ trees/max-depth/maxDepth.ts
@@ -6,10 +6,12 @@ import { TreeNodeNum } from "../common/tree";
  **/
 
 function maxDepth(node: TreeNodeNum | null): number {
+  if (node === null) return 0;
+
   let maxNodeDepth = 0;
-  if(node === null) return 0;
-  for(const child of node.children){
-    maxNodeDepth = Math.max(maxDepth(child), maxNodeDepth);
+  for (const child of node.children) {
+    const childDepth = maxDepth(child);
+    maxNodeDepth = Math.max(childDepth, maxNodeDepth);
   }
 
   return maxNodeDepth + 1;
